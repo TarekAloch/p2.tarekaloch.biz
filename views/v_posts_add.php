bcd269b4dbaa457c5fd358993a2dddb3c9343f8d
TarekAloch <taloch@fas.harvard.edu>
2013-11-11 19:46:08 -0500

    adding +1 features to  delete post

---
+++ views/v_posts_add.php
@@ -0,0 +1,9 @@
+<form method='POST' action='/posts/p_add'>
+<!-- form for adding a new post -->	
+	
+	<cite>Post your thoughts on something</cite>
+	<textarea maxlength='255' name='content' id='content' class='textarea' required></textarea>
+	<br>
+	<input type='submit' class='button 'value='Post'>
+	
+</form>
---
+++ views/v_posts_p_add.php
@@ -0,0 +1,5 @@
+
+<!-- confirmation message -->
+<h2>Thought contributed</h2>
+
+<b>Click <a href='/posts/add'>here</a> to add another.</b>
